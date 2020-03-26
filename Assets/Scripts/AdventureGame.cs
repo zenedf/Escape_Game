1a202d7d427685af9bd7c4a5b879a252b43784ea
Ethan Frazier <zenedf@protonmail.com>
2020-03-26 05:40:17 -0400

    initializing repository

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,74 @@
+﻿/// TODO
+/// [ ] Make this program a choose your own 'quest' game template.
+/// [ ] Allow PC users to click on the answer they want. (This will help when I make this game for mobile)
+/// [ ] Make this game for android
+/// [X] Make the UI dynamic
+/// 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text storyTextComponent;
+    [SerializeField] Text headerTextComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    //int[] oddNumbers = { 1, 3, 5, 7, 9 }; // Array Example
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState; // It starts in the starting state
+
+        FillInStateText();
+
+        //textComponent.text = ("This text is added programmatically as a test example!");
+        //Debug.Log(oddNumbers[3]); // Array Example...printing it
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+            FillInStateText();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+            FillInStateText();
+        }
+        //else if (Input.GetKeyDown(KeyCode.T))  // This is just for testing. Delete before sending out the game.
+        //{
+        //    state = nextStates[2];
+        //    FillInStateText();
+        //}
+
+    }
+
+    /// <summary>
+    /// This will fill in the header and story text for each state.
+    /// </summary>
+    void FillInStateText()
+    {
+        storyTextComponent.text = state.GetStateStory();
+        headerTextComponent.text = state.GetStateHeader();
+    }
+    
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This allows you to right-click and createa new State object in Unity
+[CreateAssetMenu(menuName ="State")]
+public class State : ScriptableObject
+{
+    // 1st # = minimum, 2nd # = number before we start to scroll
+    [TextArea(14, 10)] [SerializeField] string storyText;
+
+    // Maybe use a dictionary or something for the header part.
+    //   The way I'm doing this might not be adequate. (Me tend be dumb)
+    [SerializeField] string headerText;
+
+    [SerializeField] State[] nextStates;
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    /// <returns>The story text</returns>
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    /// <returns>The title of the place you are</returns>
+    /// <notes>
+    /// This method could used for debugging.
+    /// I'm assuming you would usually just have a static header that is the title of the game.
+    /// </notes>
+    public string GetStateHeader()
+    {
+        return headerText;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    /// <returns>TODO</returns>
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
